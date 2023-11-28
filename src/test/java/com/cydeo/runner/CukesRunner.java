892b870a125e9b27fa37d9606b52edf6387bb824
OlekUk <vashchu.olek@gmail.com>
2023-11-28 13:13:17 -0600

    Initial commit

---
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.cydeo.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+          "html:target/cucumber-reports.html",
+          "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/step_definition",
+        dryRun = false,
+        tags = ""
+
+
+)
+
+public class CukesRunner {
+}
